4a68fa0e0d953933a88bd3e79494b31dd9f5e456
zidanmahdavikia <mahdavikiazidan7@gmail.com>
2024-05-23 22:34:09 +0700

    detail activity

--- app/src/main/java/com/example/querizz_app/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/example/querizz_app/presentation/detail/DetailActivity.kt
@@ -1,13 +1,20 @@
 package com.example.querizz_app.presentation.detail
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.example.querizz_app.R
+import com.example.querizz_app.databinding.ActivityDetailBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +24,25 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
+
+        for (i in 0..3) {
+            val titleTv = binding
+        }
+    }
+
+    companion object {
+        private val TAB_TITLES = intArrayOf(
+            R.layout.tab_title
+        )
     }
 }
