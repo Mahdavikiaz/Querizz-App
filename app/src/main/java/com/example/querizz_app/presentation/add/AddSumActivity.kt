26a5b41a23edba7f1af2b9e303fa0cb07cd1f86a
zidanmahdavikia <mahdavikiazidan7@gmail.com>
2024-06-09 20:56:44 +0700

    event listener 1

--- app/src/main/java/com/example/querizz_app/presentation/add/AddSumActivity.kt
+++ app/src/main/java/com/example/querizz_app/presentation/add/AddSumActivity.kt
@@ -1,21 +1,33 @@
 package com.example.querizz_app.presentation.add
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.querizz_app.R
+import com.example.querizz_app.databinding.ActivityAddSumBinding
+import com.example.querizz_app.presentation.result.ResultActivity
 
 class AddSumActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddSumBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add_sum)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityAddSumBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.fileUpload.setOnClickListener {
+
+        }
+
+        binding.summarize.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/querizz_app/presentation/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/querizz_app/presentation/welcome/WelcomeActivity.kt
@@ -1,21 +1,34 @@
 package com.example.querizz_app.presentation.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.querizz_app.R
+import com.example.querizz_app.databinding.ActivityWelcomeBinding
+import com.example.querizz_app.presentation.login.LoginActivity
+import com.example.querizz_app.presentation.register.RegisterActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSignup.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
+        binding.btSignin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
